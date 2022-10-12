9eacb9da90ab971d03260a7086f59dc3ff39ea31
Rithvik-lab <85850826+Rithvik-lab@users.noreply.github.com>
2022-10-12 16:00:45 -0700

    Add files via upload

---
+++ Move_File.py
@@ -0,0 +1,7 @@
+import os
+import shutil
+
+path = "C:\\Users\\rithv\\OneDrive\\Desktop\\coding\\project 20\\cloud.png"
+destination = "C:\\Users\\rithv\\OneDrive\\Desktop\\coding\\project 38"
+des = shutil.move(path,destination)
+
